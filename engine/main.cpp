7bd3945c6d8ec13b67a9e06b92fd3f365a60445c
Mohammad Moradi <75095946+itismoradi@users.noreply.github.com>
2022-03-28 04:26:32 +0430

    complete get input from user in main function edit git ignore file and cmake

--- engine/main.cpp
+++ engine/main.cpp
@@ -3,7 +3,32 @@
     automatic creation process of the hengam calendar
 */
 
+#include <iostream>
+#include <exception>
+#include "engine.hpp"
+using namespace std;
+
 int main()
 {
+    short startJalaliYear;
+    short endJalaliYear;
+
+    cout << "hengam engine" << endl;
+    cout << "Enter << start >> year in jalali calendar : ";
+    cin >> startJalaliYear;
+    cout << "Enter << end >> year in jalali calendar : ";
+    cin >> endJalaliYear;
+
+    Engine engine(startJalaliYear, endJalaliYear);
+    
+    try
+    {
+        engine.turnOn();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     return 0;
 }
