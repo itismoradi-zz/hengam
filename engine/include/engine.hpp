3e4507f97b3d804bb5425ab8103e0c58fb5748ef
Mohammad Moradi <75095946+itismoradi@users.noreply.github.com>
2022-04-06 03:01:17 +0430

    add print functions in Engine class complete Engine turnOn function edit Engine constructor

--- engine/include/engine.hpp
+++ engine/include/engine.hpp
@@ -2,17 +2,21 @@
 #define ENGINE_IG
 
 #include <date.hpp>
+#include <fstream>
 
 class Engine
 {
 public:
     Engine(short, short);
     void turnOn();
+    void printCalendarProperties();
+    void printSummary();
 private:
     short startJalaliYear;
     short endJalaliYear;
     Date now;
     Date final;
+    std::ofstream file;
 };
 
 #endif
--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,23 +1,51 @@
 #include "engine.hpp"
 #include <iostream>
 #include <exception>
+
 using namespace std;
 
 Engine::Engine(short start, short end)
     : startJalaliYear(start), endJalaliYear(end),
-    now(start, 1, 1, JALALI)
+    now(start, 1, 1, JALALI), final(end + 1, 1, 1, JALALI),
+    file("hengam.calendar@gmail.com.ics", ios::out | ios::trunc)
 {
-    try
-    {
-        final.setDate(Date::dateString(end, 12, 30), JALALI);
-    }
-    catch(const std::exception& e)
-    {
-        final.setDate(Date::dateString(end, 12, 29), JALALI);
-    }
+
 }
 
 void Engine::turnOn()
 {
-    
+    //print calendar properties
+    printCalendarProperties();
+
+    do
+    {   
+        //print every day event properties
+        file << "BEGIN:VEVENT" << endl;
+        file << "DTSTART;VALUE=DATE:" << endl;
+        file << "DTEND;VALUE=DATE:" << endl;
+        printSummary();
+        file << "TRANSP:TRANSPARENT" << endl;
+        file << "END:VEVENT" << endl;
+        now.gotoTomorrow();
+    }
+    while (now != final);
+
+    file << "END:VCALENDAR" << endl;
+}
+
+void Engine::printCalendarProperties()
+{
+    file << "BEGIN:VCALENDAR" << endl;
+    file << "PRODID:-//hengam//hengam Calendar//EN" << endl;
+    file << "VERSION:2.0" << endl;
+    file << "CALSCALE:GREGORIAN" << endl;
+    file << "METHOD:PUBLISH" << endl;
+    file << "X-WR-CALNAME:هنگام" << endl;
+    file << "X-WR-TIMEZONE:Asia/Tehran" << endl;
+    file << "X-WR-CALDESC:تقویم خورشیدی با روز های هفته و مناسبت ها" << endl;
+}
+
+void Engine::printSummary()
+{
+    file << "SUMMARY:" << now.year() << "/" << now.month() << "/" << now.day() << endl;
 }
